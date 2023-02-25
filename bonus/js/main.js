a77a0d910bd0fdf493adef334a6662604a2aeebd
Paolo Giovannini <giovannini13@gmail.com>
2023-02-25 17:17:53 +0100

    bonus concluso

---
+++ bonus/js/main.js
@@ -0,0 +1,24 @@
+const containerDom = document.querySelector('.container');
+
+for (let i = 1; i <= 100; i++ ) {
+    const quadratino = document.createElement('div');
+    quadratino.classList.add('square');
+    
+    
+    if (i % 3 == 0 && i % 5 == 0){
+        quadratino.append("fizzbuzz");
+        quadratino.classList.add('blu');
+    } else if (i % 5 == 0){
+        quadratino.append("buzz");
+        quadratino.classList.add('grey');
+    }else if (i % 3 == 0){
+        quadratino.append("fizz");
+        quadratino.classList.add('red');
+    }else{
+        quadratino.append(i);
+        quadratino.classList.add('green');
+    }
+    
+    containerDom.append(quadratino);
+}
+
